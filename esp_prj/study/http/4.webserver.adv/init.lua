4e02413478c703e00328521714643cd8e6f0510e
도플광어 <gbox3d@gmail.com>
2016-01-12 16:24:32 +0900

    log

--- esp_prj/study/http/4.webserver.adv/init.lua
+++ esp_prj/study/http/4.webserver.adv/init.lua
@@ -18,7 +18,8 @@ function setupAPMode()
 
     cfg={}
     cfg.ssid="spectrum"..node.chipid();
-    cfg.pwd="12345678"
+    --cfg.pwd="12345678"
+    cfg.pwd=nil -- 암호는 없게, 요로케하면
     wifi.ap.config(cfg)
 
     cfg={}
@@ -38,16 +39,17 @@ end
 
 function setupSTMode()
     wifi.setmode(wifi.STATION)
-    wifi.sta.autoconnect(0)
-    wifi.sta.disconnect()
+    wifi.sta.autoconnect(0) --이전 세팅이 가끔끼어드는거 시러효!!
+    wifi.sta.disconnect() -- 완전히 끝내고 다시하고시퍼 ~~
 
     cfg = {ip = "192.168.9.20",netmask = "255.255.255.0",gateway="192.168.9.1"}
     wifi.sta.setip(cfg)
 
-    wifi.sta.config("esptest","",1)
+    wifi.sta.config("esptest","",
+        1 --오토모드의 부활
+    )
 
     wifi.sta.connect()
-
     --wifi.setphymode(wifi.PHYMODE_B) -- 모드 N에 비해 한바이트씩 전송하는것은 오히려 속도가 빠름
     --wifi.sleeptype(wifi.NONE_SLEEP) -- 슬립모드방지,이 옵션을 설정하지않으면 주기적으로 느려짐
 end
@@ -55,95 +57,6 @@ end
 -- Build and return a table of the http request data
 function parseHTTPReq (req)
     return string.match(req, "([A-Z]*) \/([^? ]*)")
-end
-
-function _(x) for v in pairs(x) do print(v) end end
-
---function startServer()
---    srv=net.createServer(net.TCP)
---    print("http server started")
---    srv:listen(80,function(conn)
---        conn:on("receive",function(conn,request)
---            local buf = ""
---            method, path = parseHTTPReq(request)
---            if (path == nil or path=="/" or path=="") then
---                path = "index.html"
---            end
---            print("] "..path)
---            if (string.len(path)==2) then
---                local l=string.sub(path,1,1)+0
---                local r=string.sub(path,2,2)+0
---                --print(l ..','.. r)
---                gpio.write(l,r);
---                conn:send('{"r":"ok"}')
---                --print(path.." direction")
---            else
---                print("[", method, ",", path, "]")
---                if (file.open(path, "r")) then
---                    local q=file.read()
---                    local x=""
---                    while q do
---                        x=x..(q and q or "")
---                        q=file.read()
---                    end
---                    --print(x)
---                    conn:send(x)
---                else
---                    conn:send("404")
---                end
---                file.close()
---            end
---            conn:close()
---            collectgarbage()
---        end)
---    end)
---end
-
-function AsyncTCPSender_Safe(option)
-    local test_set = false;
-    local tempBuf = "";
-    local over_size = 0;
-    return function(data)
-        local socket = option.getsocket()
-        function _send(data)
-            if test_set == true then tempBuf = tempBuf..data --print(data)
-            else
-                tempBuf = tempBuf..data
-                if tempBuf:len()>0 then
-
-                    if tempBuf:len() <= 1024 then
-                        local _data = tempBuf;tempBuf=""
-                        test_set=true
-                        socket:send(_data,
-                            function()
-                                test_set=false;
-                                if tempBuf:len()>0 then
-                                    --print("remain :  " ..tempBuf)
-                                    _send("") end
-                            end
-                        )
-                    else -- over 1024
-                        over_size = tempBuf:len() - 1024;
-                        local temp = string.sub(tempBuf,0,1023) print(temp)
-                        tempBuf:sub(1024)
-
-                        socket:send(temp,
-                            function()
-
-                                test_set=false;
-                                if tempBuf:len()>0 then
-                                    --print("remain :  " ..tempBuf)
-                                    _send("") end
-                            end
-                        )
-
-                    end
-                end
-
-            end
-        end
-        if socket~=nil then _send(data) end
-    end --function
 end
 
 --uri 디코딩
