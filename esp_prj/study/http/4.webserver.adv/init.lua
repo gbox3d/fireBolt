fd7711c28ecb16fb5db602d57eccbb29552c9f88
도플광어 <gbox3d@gmail.com>
2016-01-12 14:49:03 +0900

    webserver over 1046byte send data

--- esp_prj/study/http/4.webserver.adv/init.lua
+++ esp_prj/study/http/4.webserver.adv/init.lua
@@ -35,6 +35,23 @@ function setupAPMode()
     print("Soft AP started")
 end
 
+
+function setupSTMode()
+    wifi.setmode(wifi.STATION)
+    wifi.sta.autoconnect(0)
+    wifi.sta.disconnect()
+
+    cfg = {ip = "192.168.9.20",netmask = "255.255.255.0",gateway="192.168.9.1"}
+    wifi.sta.setip(cfg)
+    wifi.sta.config("RD2GUNPOWER","808671004",1)
+    --wifi.sta.config("esptest","",1)
+
+    wifi.sta.connect()
+
+    --wifi.setphymode(wifi.PHYMODE_B) -- 모드 N에 비해 한바이트씩 전송하는것은 오히려 속도가 빠름
+    --wifi.sleeptype(wifi.NONE_SLEEP) -- 슬립모드방지,이 옵션을 설정하지않으면 주기적으로 느려짐
+end
+
 -- Build and return a table of the http request data
 function parseHTTPReq (req)
     return string.match(req, "([A-Z]*) \/([^? ]*)")
@@ -89,7 +106,7 @@ function AsyncTCPSender_Safe(option)
     return function(data)
         local socket = option.getsocket()
         function _send(data)
-            if test_set == true then tempBuf = tempBuf..data --print(tempBuf)
+            if test_set == true then tempBuf = tempBuf..data --print(data)
             else
                 tempBuf = tempBuf..data
                 if tempBuf:len()>0 then
@@ -107,7 +124,7 @@ function AsyncTCPSender_Safe(option)
                         )
                     else -- over 1024
                         over_size = tempBuf:len() - 1024;
-                        local temp = string.sub(tempBuf,0,1023)
+                        local temp = string.sub(tempBuf,0,1023) print(temp)
                         tempBuf:sub(1024)
 
                         socket:send(temp,
@@ -120,11 +137,9 @@ function AsyncTCPSender_Safe(option)
                             end
                         )
 
-
-
-
                     end
                 end
+
             end
         end
         if socket~=nil then _send(data) end
@@ -132,35 +147,89 @@ function AsyncTCPSender_Safe(option)
 end
 
 --uri 디코딩
-local function unescape(s)
-    local rt, i, len = "", 1, #s
-    s = s:gsub('+', ' ')
-    local j, xx = s:match('()%%(%x%x)', i)
-    while j do
-        rt = rt .. s:sub(i, j-1) .. string.char(tonumber(xx,16))
-        i = j+3
-        j, xx = s:match('()%%(%x%x)', i)
-    end
-    return rt .. s:sub(i)
+--local function unescape(s)
+--    local rt, i, len = "", 1, #s
+--    s = s:gsub('+', ' ')
+--    local j, xx = s:match('()%%(%x%x)', i)
+--    while j do
+--        rt = rt .. s:sub(i, j-1) .. string.char(tonumber(xx,16))
+--        i = j+3
+--        j, xx = s:match('()%%(%x%x)', i)
+--    end
+--    return rt .. s:sub(i)
+--end
+
+function unescape (s)
+    s = string.gsub(s, "+", " ")
+    s = string.gsub(s, "%%(%x%x)", function (h)
+        return string.char(tonumber(h, 16))
+    end)
+    return s
 end
+
+function parseurl (s,param)
+    for k, v in string.gmatch( s, "([^&=?]+)=([^&=?]+)" ) do
+        --t[k] = v
+        if k == param then
+            --print (k.." "..v)
+            return unescape(v)
+        end
+    end
+end
+
+
+function sendLongFile(option)
+
+    local path = option.path;
+    local conn = option.socket;
+
+    if (file.open(path, "r")) then
+
+        function _loop()
+            local q=file.read(1024)
+            print(q)
+            if q ~= nil then
+                conn:send(q,_loop)
+            else
+                file.close()
+                option.callback()
+                --conn:close()
+            end
+        end
+
+        _loop()
+
+    else
+        conn:send("404",function() conn:close() end)
+        file.close()
+    end
+
+end
+
 
 function startServer()
     srv=net.createServer(net.TCP)
     print("http server started")
+
     srv:listen(80,function(conn)
         conn:on("receive", function(conn, payload)
-
             _, _, method, path, vars = string.find(payload, "([A-Z]+) /([^?]*)%??(.*) HTTP");
-            print(path);
-            print(unescape(vars));
+            if path == "json" then
+                --http://192.168.9.20/cmd?json={"fn":"gpio","val":[1,0,1]}
+                local json = parseurl(vars,"data")
+                print(json)
+                local jsn_obj = cjson.decode(json)
+                print(jsn_obj.fn);
 
-            if path == "cmd" then
-                --gpio.write(newGPIO[14],gpio.HIGH)
                 --크로스도메인 허용하기위한 헤더
-                conn:send('HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin:*\r\n\r\n' .. '{"result":"ok"}')
-                conn:close()
-            else
+                conn:send('HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin:*\r\n\r\n' .. '{"result":"ok"}',function() conn:close() end)
 
+            else
+                sendLongFile({
+                    path = path,
+                    socket = conn,
+                    callback = function() conn:close() end
+                })
 
             end
 
@@ -171,5 +240,9 @@ function startServer()
 end
 
 initGPIO()
-setupAPMode()
+--setupAPMode()
+setupSTMode()
 startServer()
+
+remaining, used, total=file.fsinfo()
+print("\nFile system info:\nTotal : "..total.." Bytes\nUsed : "..used.." Bytes\nRemain: "..remaining.." Bytes\n")
