bcc4e1cb14bd4c07b5ccf30a6133f059b03c96d1
도플광어 <gbox3d@gmail.com>
2016-01-18 10:57:41 +0900

    ...

--- esp_prj/study/basic/ap_exam1/init.lua
+++ esp_prj/study/basic/ap_exam1/init.lua
@@ -1,7 +1,11 @@
 wifi.setmode(wifi.SOFTAP)
 --패스워드를 8자이상입력하지않으면 ssid 가 ESP8266_XXX이런식으로 설정됨
-wifi.ap.config({ssid="esptest",pwd="123456789"})
+--wifi.ap.config({ssid="esptest",pwd="123456789"})
+
+-- nil 을 주면 패스워드 설정안해도됨
+wifi.ap.config({ssid="esptest",pwd=nil})
 wifi.ap.setip({ip="192.168.9.1",netmask="255.255.255.0",gateway="192.168.9.1"})
+
 print("ap mode test")
 tmr.alarm(0, 1000, 0, function() print("hello world") end )
 
--- esp_prj/study/http/1.exam.simple/init.lua
+++ esp_prj/study/http/1.exam.simple/init.lua
@@ -1,5 +1,5 @@
 wifi.setmode(wifi.STATION)
-cfg = {ip = "192.168.0.20",netmask = "255.255.255.0",gateway="192.168.0.1"}
+cfg = {ip = "192.168.9.20",netmask = "255.255.255.0",gateway="192.168.9.1"}
 wifi.sta.setip(cfg)
 wifi.sta.config("RD2GUNPOWER","808671004")
 wifi.sta.connect()
@@ -9,5 +9,6 @@ srv:listen(80,function(conn)
         _, _, method, path, vars = string.find(payload, "([A-Z]+) /([^?]*)%??(.*) HTTP");print(path)
         conn:send("<h1> Hello, NodeMCU.</h1>")
         conn:close()
+        print(node.heap());
     end)
 end)
