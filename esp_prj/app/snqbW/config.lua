5e9fb6f8186daf8a29c2b7162d3dda47a8c9bbdb
도플광어 <gbox3d@gmail.com>
2016-11-25 16:56:23 +0900

    snqbw readme.md

--- esp_prj/app/snqbW/config.lua
+++ esp_prj/app/snqbW/config.lua
@@ -1,3 +1,3 @@
 app_config = {
-    ssid ="",passwd = "",ip={192,168,9,9},bc_port=1999,recv_port=2012,baud=115200,dhcp=true,apmode=false
+    ssid ="",passwd = "",ip={192,168,9,9},bc_port=1999,recv_port=2012,baud=115200,dhcp=true,apmode=true
 }
