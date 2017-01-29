30088e8495e6857a66a398596a80870964ae9702
도플광어 <gbox3d@gmail.com>
2017-01-30 08:49:52 +0900

    ...

--- esp_prj/app/snqbW/extention/basic/ext.lua
+++ esp_prj/app/snqbW/extention/basic/ext.lua
@@ -5,7 +5,13 @@ function ext_main(delay)
     print("extention main start") 
     tmr.create():alarm(delay,tmr.ALARM_SINGLE, 
     function() 
+    local prev_status = boot_status.process;
+    boot_status.process = 'stub_check'
+    save_BootStatus();
     --dofile("")
+
+    boot_status.process = prev_status
+    save_BootStatus();
     
     end)
 end
