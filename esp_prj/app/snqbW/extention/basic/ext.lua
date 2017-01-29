aa08d3a7bf40bba66e36ed8e1b6829cf3e2c996e
도플광어 <gbox3d@gmail.com>
2017-01-29 20:00:09 +0900

    ....

--- esp_prj/app/snqbW/extention/basic/ext.lua
+++ esp_prj/app/snqbW/extention/basic/ext.lua
@@ -1,4 +1,11 @@
 print('load extention')
 --dofile("")
 
-function ext_main() print("extention main start") end
+function ext_main(delay) 
+    print("extention main start") 
+    tmr.create():alarm(delay,tmr.ALARM_SINGLE, 
+    function() 
+    --dofile("")
+    
+    end)
+end
