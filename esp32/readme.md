c9bd6f0d12b056353df4e084fbf526f9641b97ff
gbox3d <gbox3d@gmail.com>
2023-03-10 18:37:16 +0900

    esp32 btserial

---
+++ esp32/readme.md
@@ -0,0 +1,15 @@
+# esp32 예제  
+
+## example list
+
+ex01 : esp32에서 tonkey 라이브러리를 사용하여 tonkey를 제어하는 예제입니다.  
+ex02 : esp32에서 bluetooth serial 를 사용하는 예제입니다.  
+
+## 참고자료
+
+tonkey : https://github.com/gbox3d/tonkey  
+
+## 라이브러리 설치
+위에서 받은 tonkey 라이브러리를 다음과 같이 추가 합니다.  
+Arduino IDE에서 라이브러리 추가: Arduino IDE에서 "스케치" > "라이브러리 포함하기" > "zip 라이브러리 추가"를 선택합니다.  
+
---
+++ esp32/temp/sketch/ex02.ino.cpp
@@ -0,0 +1,54 @@
+#include <Arduino.h>
+#line 1 "C:\\Users\\user\\Desktop\\work\\visionApp\\fireBolt\\esp32\\example\\ex02\\ex02.ino"
+#include <BluetoothSerial.h>
+#include <BTAddress.h>
+#include <BTAdvertisedDevice.h>
+#include <BTScan.h>
+
+BluetoothSerial SerialBT;
+
+#line 8 "C:\\Users\\user\\Desktop\\work\\visionApp\\fireBolt\\esp32\\example\\ex02\\ex02.ino"
+void setup();
+#line 19 "C:\\Users\\user\\Desktop\\work\\visionApp\\fireBolt\\esp32\\example\\ex02\\ex02.ino"
+void loop();
+#line 8 "C:\\Users\\user\\Desktop\\work\\visionApp\\fireBolt\\esp32\\example\\ex02\\ex02.ino"
+void setup() {
+  Serial.begin(115200);
+  
+  SerialBT.setPin("1234"); // Bluetooth PIN code
+  SerialBT.begin("ESP32_mytest"); // Bluetooth device name
+  
+  Serial.println("Bluetooth device is ready to pair");
+  
+}
+
+int gFSM = 0;
+void loop() {
+    switch(gFSM) {
+        case 0: 
+        {
+            if (SerialBT.connected()) { // 연결된 기기가 있으면
+                Serial.println("Bluetooth Device Connected");
+                SerialBT.println("Welcome to ESP32");
+                gFSM = 1;
+            }
+        }
+        break;
+        case 1: 
+        {
+            if (SerialBT.available()) { // 연결된 기기로부터 데이터가 들어오면
+                Serial.write(SerialBT.read()); // 시리얼 모니터에 출력
+            }
+            if (Serial.available()) { // 시리얼 모니터로부터 데이터가 들어오면
+                SerialBT.write(Serial.read()); // 연결된 기기로 출력
+            }
+            if (!SerialBT.connected()) { // 연결된 기기가 없으면
+                Serial.println("Bluetooth Device Disconnected");
+                gFSM = 0;
+            }
+        }
+    }
+
+  delay(20);
+}
+
