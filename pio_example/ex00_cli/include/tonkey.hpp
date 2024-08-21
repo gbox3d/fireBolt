b3706def7faa3e48736aa4f243ff1e816e76053b
gbox3d <gbox3d@gmail.com>
2024-08-21 10:15:42 +0900

    cli example parseCmd 분리

--- pio_example/ex00_cli/include/tonkey.hpp
+++ pio_example/ex00_cli/include/tonkey.hpp
@@ -3,55 +3,32 @@
 
 #define MAXTOKENS 64
 
-
-class tonkey
-{
+class tonkey {
 private:
-    /* data */
     String mTokens[MAXTOKENS];
     int mTokenCount = 0;
-    
-    
+
 public:
-    
-    tonkey(/* args */);
-    ~tonkey();
+    tonkey() {}
+    ~tonkey() {}
 
-    void parse(String _strLine);
-
-    int getTokenCount() { return mTokenCount; }
-    String getToken(int _index) { return mTokens[_index]; }
-
-};
-
-tonkey::tonkey(/* args */)
-{
-}
-
-tonkey::~tonkey()
-{
-}
-
-void tonkey::parse(String _strLine)
-{
-    int StringCount = 0;
-    // parse command
-    while (_strLine.length() > 0)
-    {
-        int index = _strLine.indexOf(' ');
-        if (index == -1) // No space found
-        {
-            mTokens[StringCount++] = _strLine;
-            break;
+    inline void parse(String _strLine) {
+        int StringCount = 0;
+        while (_strLine.length() > 0) {
+            int index = _strLine.indexOf(' ');
+            if (index == -1) {
+                mTokens[StringCount++] = _strLine;
+                break;
+            } else {
+                mTokens[StringCount++] = _strLine.substring(0, index);
+                _strLine = _strLine.substring(index + 1);
+            }
         }
-        else
-        {
-            mTokens[StringCount++] = _strLine.substring(0, index);
-            _strLine = _strLine.substring(index + 1);
-        }
+        mTokenCount = StringCount;
     }
 
-    mTokenCount = StringCount;
-}
+    inline int getTokenCount() { return mTokenCount; }
+    inline String getToken(int _index) { return mTokens[_index]; }
+};
 
 #endif // TONKEY_HPP
